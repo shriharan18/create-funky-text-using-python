6ca59739b4a750457df7a760c7d6307cc836ea12
Shriharan <74127448+tonyshree18@users.noreply.github.com>
2021-06-04 13:13:20 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,7 @@
+import pyfiglet
+
+ascii_banner = pyfiglet.figlet_format("python")
+
+print(ascii_banner)
+
+# This is the code...
---
+++ main.py
@@ -0,0 +1,7 @@
+from pyfiglet import Figlet
+
+custom_fig = Figlet(font='puffy')
+
+print(custom_fig.renderText("Python"))
+
+# this is the code...
